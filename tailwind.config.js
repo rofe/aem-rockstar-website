40c19bdbeb1316d04bb544e8dc13f27442a96844
Sean Steimer <ssteimer@adobe.com>
2022-07-27 10:38:09 -0700

    init tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./blocks/**/*.js", "./scripts/**/*.js", "./head.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
